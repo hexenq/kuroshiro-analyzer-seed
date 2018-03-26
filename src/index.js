17b3a3d7f49bd0608560f4c6bd437153adfe7548
hexenq <hexenq@gmail.com>
2018-03-26 18:13:43 +0800

    20180326

--- src/index.js
+++ src/index.js
@@ -1,39 +1,51 @@
-/**
- * Check where we are
- * @returns {boolean} if we are in node.js
- */
-var isNode = function () {
-    try { return this === global; } catch (e) { return false; }
-};
-
-function Analyzer({ dicPath } = {}) {
-    this._analyzer = null;
-
-    // Variable Statement
-    // ...
-    //  if(isNode()) { ... }
-    //  else { ... }
-    // ...
-}
-
-Analyzer.prototype.init = function (callback) {
-    let self = this;
-    if (this._analyzer == null) {
-        kuromoji.builder({ dicPath: this._dicPath }).build(function (err, newAnalyzer) {
-            if (err)
-                return callback(err);
-
-            self._analyzer = newAnalyzer;
-            callback();
-        });
-    } else {
-        callback(new Error("This analyzer has already been initialized."));
+class Analyzer {
+    constructor() {
+        this.isInitialized = false;
+        // Variable Statement
+        // ...
+        //  if(isNode) { ... }
+        //  else { ... }
+        // ...
     }
-};
 
-Analyzer.prototype.parse = function (str, callback) {
-    if (str.trim() == "") return callback(null, str);
-    callback(null, this._analyzer.tokenize(str));
-};
+    init(callback = () => {}) {
+        if (this.isInitialized == false) {
+            // Initialize the analyzer
+            // ...
+            // When finished, call back
+            this.isInitialized = true;
+            callback();
+        } else {
+            callback(new Error("This analyzer has already been initialized."));
+        }
+    }
+
+    /**
+     * Parse the input string
+     * @param {*} str input string
+     * @param {*} callback callback function invoked after parsing
+     * @example output
+     * {
+     *     surface_form: '黒白',    // [Required] 表層形
+     *     pos: '名詞',               // 品詞 (part of speech)
+     *     pos_detail_1: '一般',      // 品詞細分類1
+     *     pos_detail_2: '*',        // 品詞細分類2
+     *     pos_detail_3: '*',        // 品詞細分類3
+     *     conjugated_type: '*',     // 活用型
+     *     conjugated_form: '*',     // 活用形
+     *     basic_form: '黒白',      // 基本形
+     *     reading: 'クロシロ',       // [Required] 読み
+     *     pronunciation: 'クロシロ'  // 発音
+     * }
+     */
+    parse(str = "", callback = () => {}) {
+        // Parse the input string
+        // ...
+        callback(null, {
+            surface_form: '黒白',
+            reading: 'クロシロ'
+        });
+    }
+}
 
 export default Analyzer;
